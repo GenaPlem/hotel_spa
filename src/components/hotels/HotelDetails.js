1aa69cdd8b64d6dcd2f23a515de6db8d5f31d7bd
GenaPlem <shainkill1337@gmail.com>
2024-12-19 18:15:41 +0000

    Add Hotel Detail data

--- src/components/hotels/HotelDetails.js
+++ src/components/hotels/HotelDetails.js
@@ -14,7 +14,43 @@ export default function HotelDetails() {
 
   return (
     <>
+      <img src={hotelData.imageUrl} alt="" />
       <h1 className="my-10 text-2xl sm:text-3xl">{hotelData.name}</h1>
+      <div className="flex justify-between">
+        <p className="text-left text-xl">
+          Hotel located in{" "}
+          <span className="font-bold">{hotelData.location}</span>
+        </p>
+        <p>
+          Rating: <span className="font-bold">{hotelData.rating}/5</span>
+        </p>
+      </div>
+      <p className="text-left py-3 font-bold">{hotelData.boardBasis}</p>
+      <div className="my-5 flex flex-wrap justify-between gap-2">
+        <div className="flex gap-4">
+          <p>Available dates: </p>
+          {hotelData.datesOfTravel.map((date, index) => (
+            <p
+              key={`date-${index}`}
+              className="block p-1 text-xs text-black rounded-md sm:text-sm bg-sky-400"
+            >
+              {date}
+            </p>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          <p className="font-bold">Rooms:</p>
+
+          {hotelData.rooms.map((room, index) => (
+            <span
+              key={`room-${index}`}
+              className="block font-b p-1 text-xs text-black rounded-md sm:text-sm bg-sky-400"
+            >
+              {room.roomType}: <span className="font-bold">{room.amount}</span>
+            </span>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
