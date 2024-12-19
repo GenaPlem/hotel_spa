95e3d27b4ec76bc9bbe05475a2661029a9503237
GenaPlem <shainkill1337@gmail.com>
2024-12-19 12:26:58 +0000

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className=""></div>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HotelsPage from "./pages/HotelsPage";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hotels" element={<HotelsPage />} />
+      </Routes>
+    </Router>
+  );
+}
