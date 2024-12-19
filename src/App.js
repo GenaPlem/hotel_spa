32645c46d7c861cbb41179b9164f0dfb8a1670a0
GenaPlem <shainkill1337@gmail.com>
2024-12-19 15:16:37 +0000

    Add dynamic route for hotel details page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Header from "./components/layout/Header";
 import HotelsPage from "./pages/HotelsPage";
+import DetailsPage from "./pages/DetailsPage";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/hotels" element={<HotelsPage />} />
+        <Route path="/hotels/:id" element={<DetailsPage />} />
         <Route path="*" element={<Navigate to="/hotels" replace />} />
       </Routes>
     </Router>
