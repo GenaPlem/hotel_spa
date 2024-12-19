68e4b77f7de9f729b54695a23dcf92e8ae486004
GenaPlem <shainkill1337@gmail.com>
2024-12-19 18:16:03 +0000

    Update Hotels List styles

--- src/components/hotels/HotelsList.js
+++ src/components/hotels/HotelsList.js
@@ -3,7 +3,7 @@ import HotelCard from "./HotelCard";
 export default function HotelsList({ hotels }) {
   return (
     <section>
-      <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+      <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 lg:grid-cols-4">
         {hotels.map((hotel) => (
           <HotelCard key={hotel.id} hotel={hotel} />
         ))}
