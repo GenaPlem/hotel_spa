e8b2c8f5ac932ac8ded40bb6b1aa79321a56b714
GenaPlem <shainkill1337@gmail.com>
2024-12-19 18:30:07 +0000

    Remove unused state from Hotels Page

--- src/pages/HotelsPage.js
+++ src/pages/HotelsPage.js
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import HotelsList from "../components/hotels/HotelsList";
 import hotelsData from "../data/hotels.json";
 
 export default function HotelsPage() {
-  const [hotels, setHotels] = useState(hotelsData);
-
   return (
     <>
       <h1 className="text-center text-3xl font-semibold my-9 md:text-5xl">
         Hotels
       </h1>
-      <HotelsList hotels={hotels} />
+      <HotelsList hotels={hotelsData} />
     </>
   );
 }
