7b90e4c1feb04216e46f16304e48dd779bc5513b
GenaPlem <shainkill1337@gmail.com>
2024-12-19 18:15:05 +0000

    Update styles for Hotel Card component

--- src/components/hotels/HotelCard.js
+++ src/components/hotels/HotelCard.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function HotelCard({ hotel }) {
   return (
-    <article className="relative p-2 flex flex-col justify-between overflow-hidden transition duration-300 transform border-sky-500 border rounded-xl hover:scale-105">
+    <article className="relative p-2 md:p-8 flex flex-col overflow-hidden transition duration-300 transform border-sky-500 border rounded-xl hover:scale-105">
       <div>
         <img
           src={hotel.imageUrl}
@@ -13,31 +13,35 @@ export default function HotelCard({ hotel }) {
           {hotel.name}
         </h2>
       </div>
-
       <p className="mb-2 font-bold">{hotel.location}</p>
-      <p className="mb-2 text-xs font-bold">Rate: {hotel.rating}/5</p>
-
-      <p className="mb-2 text-xs font-bold">
-        Available Dates:
-        <br />
-        {hotel.datesOfTravel.map((date, index) => (
-          <span key={`date-${index}`} className="block pt-1 font-normal">
-            {date}
+      <div className="text-left">
+        <div className="flex justify-between pt-5 gap-2">
+          <p className="mb-2 text-xs font-bold">{hotel.boardBasis}</p>
+          <p className="mb-2 text-xs font-bold">Rate: {hotel.rating}/5</p>
+        </div>
+        <div className="flex flex-wrap justify-between pt-5 gap-5">
+          <p className="mb-2 text-xs font-bold">
+            Dates:
             <br />
-          </span>
-        ))}
-      </p>
-      <p className="mb-2 text-xs font-bold">{hotel.boardBasis}</p>
-      <p className="mb-2 text-xs font-bold">
-        Available Rooms:
-        <br />
-        {hotel.rooms.map((room, index) => (
-          <span key={`room-${index}`} className="block pt-1 font-normal">
-            {room.roomType}: {room.amount}
+            {hotel.datesOfTravel.map((date, index) => (
+              <span key={`date-${index}`} className="block pt-1 font-normal">
+                {date}
+                <br />
+              </span>
+            ))}
+          </p>
+          <p className="mb-2 text-xs font-bold">
+            Rooms:
             <br />
-          </span>
-        ))}
-      </p>
+            {hotel.rooms.map((room, index) => (
+              <span key={`room-${index}`} className="block pt-1 font-normal">
+                {room.roomType}: {room.amount}
+                <br />
+              </span>
+            ))}
+          </p>
+        </div>
+      </div>
       <Link
         to={`/hotels/${hotel.id}`}
         className="absolute inset-0 z-10"
