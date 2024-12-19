70db95d5cd7f8f5ac705b4ce30ad7aff58328716
GenaPlem <shainkill1337@gmail.com>
2024-12-19 15:03:48 +0000

    Remove console.log

--- src/components/hotels/HotelsList.js
+++ src/components/hotels/HotelsList.js
@@ -1,12 +1,11 @@
 import HotelCard from "./HotelCard";
 
 export default function HotelsList({ hotels }) {
-  console.log(hotels);
   return (
     <section>
       <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {hotels.map((hotel) => (
-          <HotelCard key={hotel.id} hotel />
+          <HotelCard key={hotel.id} hotel={hotel} />
         ))}
       </div>
     </section>
