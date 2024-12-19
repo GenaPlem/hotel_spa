10344f3961e21c94341a5c3e77268df680916073
GenaPlem <shainkill1337@gmail.com>
2024-12-19 16:14:13 +0000

    Add Hotel data for Hotel Details component

--- src/components/hotels/HotelDetails.js
+++ src/components/hotels/HotelDetails.js
@@ -1,7 +1,17 @@
 import { useParams } from "react-router-dom";
+import hotelsData from "../../data/hotels.json";
 
 export default function HotelDetails() {
   const { id } = useParams();
+  console.log(hotelsData);
 
-  return <p>{id}</p>;
+  const hotelData = hotelsData.find((hotel) => hotel.id.toString() === id);
+
+  console.log(hotelData);
+
+  return (
+    <>
+      <h1 className="my-10 text-2xl sm:text-3xl">{hotelData.name}</h1>
+    </>
+  );
 }
