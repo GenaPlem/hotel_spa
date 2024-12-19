501cb3ba646873a976e785c8884bdcca37fab264
GenaPlem <shainkill1337@gmail.com>
2024-12-19 12:57:14 +0000

    Update App with Header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import Header from "./components/layout/Header";
 import HotelsPage from "./pages/HotelsPage";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/hotels" element={<HotelsPage />} />
         <Route path="*" element={<Navigate to="/hotels" replace />} />
