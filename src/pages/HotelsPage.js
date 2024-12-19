f00f3e6a886889154d46081add67bd27290d8582
GenaPlem <shainkill1337@gmail.com>
2024-12-19 13:55:54 +0000

    Add Hotels data from json to Hotels Page

--- src/pages/HotelsPage.js
+++ src/pages/HotelsPage.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import HotelsList from "../components/hotels/HotelsList";
+import hotelsData from "../data/hotels.json";
+
 export default function HotelsPage() {
+  const [hotels, setHotels] = useState(hotelsData);
+
   return (
-    <h1 className="text-center text-3xl font-semibold my-9 md:text-5xl">
-      Hotels
-    </h1>
+    <>
+      <h1 className="text-center text-3xl font-semibold my-9 md:text-5xl">
+        Hotels
+      </h1>
+      <HotelsList hotels={hotels} />
+    </>
   );
 }
