c4bf5467d452382512e8039b923070680bcd85eb
GenaPlem <shainkill1337@gmail.com>
2024-12-19 13:56:22 +0000

    Add hotels data to Hotels List

--- src/components/hotels/HotelsList.js
+++ src/components/hotels/HotelsList.js
@@ -1,13 +1,13 @@
 import HotelCard from "./HotelCard";
 
-export default function HotelsList() {
+export default function HotelsList({ hotels }) {
+  console.log(hotels);
   return (
     <section>
       <div className="grid grid-cols-2 gap-10 my-14 md:mt-20 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-        <HotelCard />
-        <HotelCard />
-        <HotelCard />
-        <HotelCard />
+        {hotels.map((hotel) => (
+          <HotelCard key={hotel.id} hotel />
+        ))}
       </div>
     </section>
   );
